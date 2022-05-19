f3d3f85e4035950ae32af13cf1e4b3e84d02c99b
Emi <92827957+emiandere@users.noreply.github.com>
2022-05-18 23:37:20 -0500

    delete

--- pluto-proj/frontend/src/contract/address.js
+++
@@ -1,3 +0,0 @@
-// sync-ed from root via `tr sync-refs`
-import config from "../refs.terrain.json"
-export const contractAddress = (wallet) => config[wallet.network.name]['cw721-metadata-onchain'].contractAddresses.default
